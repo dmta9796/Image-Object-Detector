7f4453a1957d565b585984b0638944be9fcfa772
dmta9796 <dmta9796@colorado.edu>
2019-05-24 15:28:30 -0600

    preview of image before and routing image to server

--- server/src/settings.py
+++ server/src/settings.py
@@ -37,14 +37,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 )
 
+CORS_ORIGIN_ALLOW_ALL = False
+
 MIDDLEWARE = (
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
--- server/src/views.py
+++ server/src/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return HttpResponse("Hello, world. http response")
 
+@csrf_exempt
 def data(request):
     print(request.body)
+    #c = {}
     return HttpResponse('data', request.body)
