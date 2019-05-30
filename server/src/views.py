b6fa41dcf4d09ed2d5857411bd3e5b7b114267c5
dmta9796 <dmta9796@colorado.edu>
2019-05-30 14:58:17 -0600

    resolved python imports

--- server/src/views.py
+++ server/src/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import io
+import sys
+import os
+from PIL import Image
+# sys.path.append('./PyTorch-YOLOv3')
+#import src.PyTorch-YOLOv3.models
+# from man.PyTorchYOLOv3 import models
+#from ../PyTorch-YOLOv3/models import *
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
+import src.PyTorchYOLOv3.models as models
 
 def index(request):
     return HttpResponse("Hello, world. http response")
 
 @csrf_exempt
 def data(request):
-    print(request.body)
-    return HttpResponse(request.body)
+    data = request.body
+    image = Image.open(io.BytesIO(data))
+    print(image)
+    return HttpResponse(data)
